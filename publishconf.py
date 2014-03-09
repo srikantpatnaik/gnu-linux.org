cc55cd6c147a0794c3057198582915e6d307f12b
Srikant Patnaik <srikant@gnu-linux.org>
2014-03-09 16:58:17 +0530

    added analytics info and gitignore for htaccess

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "aplaceforfreeminds"
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "UA-37371681-1"
